6451caae6f6d8da9b63d8e7e1ca4c3af5b9d969f
Anas Rashid <125889610+anasrashid-1@users.noreply.github.com>
2025-01-09 11:00:57 +0530

    Update supabase.ts

--- app/src/lib/supabase.ts
+++ app/src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Get these values from your Supabase project settings
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;  
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
